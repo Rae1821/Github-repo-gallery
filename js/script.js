4f2fee6be1a02c6b834bc0516720b2a0144fc040
Rae1821 <102691550+Rae1821@users.noreply.github.com>
2022-04-20 12:42:45 -0400

    Part 2 of repo gallery project complete

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 const profile = document.querySelector(".overview");
 //Github username
 const username = "Rae1821";
+//Targeting the unordered list to display the repos list
+const reposList = document.querySelector(".repo-list");
+
 
 //Async function to fetch info from github profile
 const profileInfo = async function () {
@@ -32,4 +35,23 @@ const userData = function(data) {
         <p><strong>Number of public repos:</strong> ${publicRepos}</p>
     </div>`;
     profile.append(userInfo);
+    fetchRepos();
+};
+
+//Async function to fetch your repos
+const fetchRepos = async function() {
+    const res2 = await fetch(`https://api.github.com/users/${username}/repos?sort=created&per_page=100/`);
+    const repos = await res2.json();
+    console.log(repos);
+    displayRepos(repos);
+};
+
+//Function to display info about your repos
+const displayRepos = function(repos) {
+    for(const list of repos) {
+       const li = document.createElement("li");
+       li.classList.add(".repo");
+       li.innerHTML = `<h3>${list.name}</h3>`;
+       reposList.append(li);
+    }
 };
